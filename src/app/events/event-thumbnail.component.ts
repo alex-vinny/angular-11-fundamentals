e0447b60fe89be17b22b5986205a854cf7ff1447
Alex Vinicius Zamboli <alex.zamboli@evup.com.br>
2022-02-19 19:15:48 -0300

    Implementação de estilo especifíco para o componente.

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,7 +2,10 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'event-thumbnail',
-  templateUrl: './event-thumbnail.component.html'
+  templateUrl: './event-thumbnail.component.html',
+  styleUrls: [
+    './event-thumbnail.component.css'
+  ]
 })
 export class EventThumbnailComponent {
   @Input() event: any
