cfc34b0d6b0d9275f93800a93c9aaf2313655e98
Alex Vinicius Zamboli <alex.zamboli@evup.com.br>
2022-02-20 13:50:32 -0300

    Recuperando o parâmetro da rota via classe ActivatedRoute.

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { EventService } from "../shared/event.services";
 
 // Route:
@@ -9,10 +10,11 @@ import { EventService } from "../shared/event.services";
 })
 export class EventDetailsComponent implements OnInit {
     event :any
-    constructor(private eventService: EventService) {        
+    constructor(private eventService :EventService, private route :ActivatedRoute) {        
     }
 
     ngOnInit(): void {
-        this.event = this.eventService.getEvent(1)
+        let id = +this.route.snapshot.params['id']
+        this.event = this.eventService.getEvent(id)
     }
 }
