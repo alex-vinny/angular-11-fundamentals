2876bfeb736f042f9782a4d879af98eede957454
Alex Vinicius Zamboli <alex.zamboli@evup.com.br>
2022-02-20 17:03:06 -0300

    Criado um novo módulo e adicionado a injeção via rota no arquivo principal.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -11,5 +11,10 @@ export const appRoutes :Routes = [
     { path: 'events', component: EventsListComponent, resolve: { events: EventListResolver } },
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouterActivator] },
     { path: '404', component: Error404Component },
-    { path: '', redirectTo: '/events', pathMatch: 'full' }
+    { path: '', redirectTo: '/events', pathMatch: 'full' },
+    {
+        path: 'user', 
+        loadChildren: () => import('./user/user.module')
+            .then(m => m.UserModule) 
+    }
 ]
