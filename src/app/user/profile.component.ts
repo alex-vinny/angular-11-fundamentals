2da1981b3db553b593ed6b2ea1291fe024f1c03c
Alex Vinicius Zamboli <alex.zamboli@evup.com.br>
2022-03-21 18:22:00 -0300

    Adicionado validator no FormControl
    Aplicado css para estilizar a mensagem de erro
    Melhorias:
    - adicionado comando "poll" para o live-load num container docker.

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,25 +1,32 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
 import { AuthService } from './auth.service'
 
 @Component({
   templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
   profileForm : FormGroup
+  firstName: FormControl
+  lastName: FormControl
   
   constructor(private authService: AuthService, private router:Router) {
+    this.firstName = new FormControl()
+    this.lastName = new FormControl()
     this.profileForm = new FormGroup({})
   }
 
   ngOnInit(): void {
-    let firstName = new FormControl(this.authService.currentUser?.firstName)
-    let lastName = new FormControl(this.authService.currentUser?.lastName)
+    this.firstName = new FormControl(
+      this.authService.currentUser?.firstName, Validators.required)
+    this.lastName = new FormControl(
+      this.authService.currentUser?.lastName, Validators.required)
     
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     })
   }
 
@@ -27,8 +34,18 @@ export class ProfileComponent implements OnInit {
     this.router.navigate(['events'])
   }
 
-  saveProfile(formValues : any) {    
-    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-    this.router.navigate(['events'])
+  saveProfile(formValues : any) {
+    if (this.profileForm.valid) {    
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+      this.router.navigate(['events'])
+    }
+  }
+
+  validateFirstName() :boolean {
+    return this.firstName.valid || this.firstName.untouched
+  }
+
+  validateLastName() : boolean {
+    return this.lastName.valid || this.lastName.untouched
   }
 }
