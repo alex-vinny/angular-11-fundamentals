57f99f8765d23c9e36bbc82d45277f1bdc77a397
Alex Vinicius Zamboli <alex.zamboli@evup.com.br>
2022-03-21 17:50:55 -0300

    Exemplo de FormGroup e FormControl

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,10 +1,34 @@
 import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
+import { Router } from '@angular/router'
+import { AuthService } from './auth.service'
 
 @Component({
   templateUrl: './profile.component.html',
 })
 export class ProfileComponent implements OnInit {
+  profileForm : FormGroup
+  
+  constructor(private authService: AuthService, private router:Router) {
+    this.profileForm = new FormGroup({})
+  }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    let firstName = new FormControl(this.authService.currentUser?.firstName)
+    let lastName = new FormControl(this.authService.currentUser?.lastName)
+    
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName
+    })
+  }
+
+  cancel() {
+    this.router.navigate(['events'])
+  }
+
+  saveProfile(formValues : any) {    
+    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+    this.router.navigate(['events'])
   }
 }
